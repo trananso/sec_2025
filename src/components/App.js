3654ec8863e0150a01114c9819770e66b8c36e80
Anson Tran <trananson@protonmail.com>
2025-11-24 16:13:32 -0500

    Frontend prototype

--- src/components/App.js
+++ src/components/App.js
@@ -3,53 +3,18 @@ import "@shoelace-style/shoelace/dist/components/textarea/textarea.js";
 import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "@shoelace-style/shoelace/dist/components/card/card.js";
 
+import "./JobBoardPage.js"
+
 export default class App extends LitElement {
   static styles = css`
-    :host {
-      display: flex;
-      justify-content: center;
-      padding: 40px;
-      background: var(--sl-color-neutral-100);
-      min-height: 100vh;
-      box-sizing: border-box;
-      font-family: var(--sl-font-sans);
-    }
-
-    .container {
-      width: 100%;
-      max-width: 600px;
-    }
-
-    sl-card {
-      padding: 20px;
-    }
-
-    .actions {
-      display: flex;
-      justify-content: flex-end;
-      margin-top: 15px;
-    }
   `;
 
-  render() {
+  render() {    
     return html`
-      <div class="container">
-        <sl-card>
-          <h2>Feedback</h2>
-
-          <sl-textarea
-            rows="6"
-            placeholder="Type something…"
-            resize="auto"
-            style="margin-top: 10px;"
-          ></sl-textarea>
-
-          <div class="actions">
-            <sl-button variant="primary">Submit</sl-button>
-          </div>
-        </sl-card>
-      </div>
-    `;
+    <sl-theme name="dark">
+        <job-board-page></job-board-page>
+    </sl-theme>
+    `;  
   }
 }
 
